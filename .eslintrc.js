e4e9235ffda006b54d1f4f48025c53c7dc8cd5d3
Haris Michelogiannakis <tmichelogiannakis@gmail.com>
2021-03-11 23:36:47 +0200

    chore: update husky hooks

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,7 @@ module.exports = {
   },
   plugins: ['react', '@typescript-eslint', 'prettier'],
   rules: {
+    'prettier/prettier': ['error'],
     'react/prop-types': 'off',
     'react/react-in-jsx-scope': 'off',
     'no-use-before-define': ['error'],
